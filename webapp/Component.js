9f6b6086939632d90e73ec58e97c331b0c3862e2
Mayank Thakur <er.mayanksap@yahoo.in>
2019-05-15 12:14:10 +0000

    safds

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("DED_BN_REPlistext.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("DED_BN_REPlistext.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
